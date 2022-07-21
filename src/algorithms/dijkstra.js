a8ee8bd67fb703c4fa8dbe45c8b15e69a983cf7f
jl118 <jen.liebelt@gmail.com>
2022-07-21 14:12:42 -0700

    worked on setting up dijkstra's algorithm

---
+++ src/algorithms/dijkstra.js
@@ -0,0 +1,20 @@
+/* Dijkstra's algorithm
+Returns all nodes in the order they were visited
+Also allows nodes to point back to the previous node to 
+compute shortest path by backtracking from the finish node */
+
+function dijkstra(grid, startNode, finishNode) {
+    const visitedNodes = [];
+    startNode.distance = 0;
+
+}
+
+// sort nodes function
+
+// update visited nodes function
+
+// get unvisited nodes function
+
+// get all nodes function
+
+// after finishNode is found, create function to return shortest path
